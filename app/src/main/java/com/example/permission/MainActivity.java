3f92f8d5e72e48b474077156fc99a4f266ed52e7
liran <liranisraeli1@gmail.com>
2022-11-15 14:18:25 +0200

    add: wifi airplane mode silent mode vibrate mode

--- app/src/main/java/com/example/permission/MainActivity.java
+++ app/src/main/java/com/example/permission/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.permission;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,7 +19,11 @@ import com.google.android.material.button.MaterialButton;
 public class MainActivity extends AppCompatActivity {
     private MaterialButton main_BTN_login;
     private EditText main_EDT_password;
+    //battery
     private static BatteryManager myBatteryManager;
+    //wifi
+    private ConnectivityManager connManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void initSystemService(Context context) {
         myBatteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
+        connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+
     }
 
 
@@ -36,18 +48,66 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String input = main_EDT_password.getText().toString();
                 int password = Integer.parseInt(input);
+                //battery equal to password
                if(getBatteryPercentage() == password){
                    Toast.makeText(MainActivity.this,"login successfully - the battery equal to password",Toast.LENGTH_SHORT).show();
+               }
+               //airplane mode is on
+               else if(isAirplaneModeOn(MainActivity.this)){
+                   Toast.makeText(MainActivity.this,"login successfully - airplane mode is open",Toast.LENGTH_SHORT).show();
+               }
+               //wifi is connect
+               else if(isWifiConnect()){
+                   Toast.makeText(MainActivity.this,"login successfully - wifi is connect",Toast.LENGTH_SHORT).show();
+               }
+               //silent mode on
+               else if(isSilentModeOn()){
+                   Toast.makeText(MainActivity.this,"login successfully - silent mode is open",Toast.LENGTH_SHORT).show();
+               }
+                //vibrate mode on
+               else if(isVibrateOn()){
+                   Toast.makeText(MainActivity.this,"login successfully - vibrate mode is open",Toast.LENGTH_SHORT).show();
                }
                else{
                    Toast.makeText(MainActivity.this,"login failed",Toast.LENGTH_SHORT).show();
                }
+
             }
         });
     }
 
     public static int getBatteryPercentage() {
          return myBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+    }
+
+    private static boolean isAirplaneModeOn(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+    }
+
+    private boolean isSilentModeOn(){
+        AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        if(am.getRingerMode() == AudioManager.RINGER_MODE_SILENT){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private boolean isVibrateOn(){
+        AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        if(am.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean isWifiConnect(){
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return mWifi.isConnected();
     }
 
 
